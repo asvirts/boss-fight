082ec04d2d963056639148b61ac288274a6e0e4e
Andrew Virts <asvirts@outlook.com>
2022-08-07 16:59:18 -0500

    layout

---
+++ index.js
@@ -0,0 +1,9 @@
+let playerHP = 50
+let bossHP = 50
+
+function generateRandom(maxLimit = 100) {
+    let rand = Math. random() * maxLimit;
+    console. log(rand); // say 99.81321410836433.
+    rand = Math. floor(rand); // 99.
+    return rand;
+}
