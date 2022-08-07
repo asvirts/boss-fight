8e1b2d912e9eb0089efede80dac328284606b8c9
Andrew Virts <asvirts@outlook.com>
2022-08-07 17:50:16 -0500

    connected js to buttons

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 let playerHP = 50
 let bossHP = 50
+document.getElementById('bossHP').innerHTML
+document.getElementById('playerHP').innerHTML
 
 
-function generateRandom(maxLimit = 100) {
-    let rand = Math. random() * maxLimit;
-    console. log(rand); // say 99.81321410836433.
-    rand = Math. floor(rand); // 99.
-    return rand;
+function normalAttack() {
+    element = document.getElementById("bossHP");
+    let newBossHP = bossHP - 3;
+    bossHP = newBossHP;
+    element.innerHTML = bossHP;
+    alert("3 damage to boss")
 }
+
+function powerAttack() {
+    element = document.getElementById("bossHP");
+    let newBossHP = bossHP - 7;
+    bossHP = newBossHP;
+    element.innerHTML = bossHP;
+    alert("7 damage to boss")
+}
+
+function defensiveAttack() {
+    element = document.getElementById("bossHP");
+    let newBossHP = bossHP - 1;
+    bossHP = newBossHP;
+    element.innerHTML = bossHP;
+    alert("1 damage to boss")
+}
+
